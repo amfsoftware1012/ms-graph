f69cd9e8c83f607e3eb5fe2f1802b261ffe0b353
Arturo Morales <arturo.morales@groupm.com>
2018-05-13 17:16:05 +0100

    Graph Console and Base Project

---
+++ GraphConsole/GraphConsole/AuthenticationHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace GraphConsole
+{
+    public static class AuthenticationHelper
+    {
+        private static readonly string ApplicationId = ConfigurationManager.AppSettings["ida:AppId"];
+
+        public static PublicClientApplication IdentityClientApp = new PublicClientApplication(ApplicationId)
+        {
+            RedirectUri = ConfigurationManager.AppSettings["ida:ReturnUrl"]
+        };
+        
+        public static async Task<string> GetTokenForUserAsync()
+        {
+            var scopes = new string[]
+            {
+                "https://graph.microsoft.com/User.ReadBasic.All",
+                "https://graph.microsoft.com/Files.ReadWrite"
+            };
+            var authResult = await IdentityClientApp.AcquireTokenAsync(scopes);
+            return authResult.AccessToken;
+        }
+    }
+}
---
+++ GraphConsole/GraphConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var token = AuthenticationHelper.GetTokenForUserAsync().Result;
+            Console.WriteLine($"Your access token is: {token}");
+            Console.ReadLine();
+            Console.WriteLine($"Call to get all files in one drive");
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var filesJsonResponse = httpClient.GetStringAsync("https://graph.microsoft.com/v1.0/me/drive/root/children").Result;
+                Console.WriteLine(filesJsonResponse);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
